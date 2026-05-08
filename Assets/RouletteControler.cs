9e3d3755639a9619bb6be7276c2fd0fcb1e6db1c
R.M. <bda07-s250215@gclass.ice.go.kr>
2026-05-08 09:23:35 +0900

    Update RouletteControler.cs

--- Assets/RouletteControler.cs
+++ Assets/RouletteControler.cs
@@ -5,22 +5,22 @@ using UnityEngine;
 public class RouletteControler : MonoBehaviour
 {
     // Start is called before the first frame update
-    float rotSpeed = 0;
+    float rotSpeed = 0; //회전 속도 초기화
 
     private void Start()
     {
-        print(rotSpeed);
+        print(rotSpeed); //회전 값 확인
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0)) //마우스를 클릭하면
         {
-            this.rotSpeed = 10;
+            this.rotSpeed = 10; //속도를 10으로 설정
         }
-        transform.Rotate(0, 0, this.rotSpeed);
-        this.rotSpeed *= 0.96f;
+        transform.Rotate(0, 0, this.rotSpeed); //속도값만큼 회전
+        this.rotSpeed *= 0.96f; //회전 속도 감소
         
     }
 }
